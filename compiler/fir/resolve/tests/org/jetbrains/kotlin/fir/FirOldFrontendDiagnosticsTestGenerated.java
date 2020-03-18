9bc63a193a1a6c3ff9cda6aa9e660adae6fe749f
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-03-18 16:23:54 +0300

    [NI] Prefer between two complex variables one with proper lower bounds
     #KT-37554 Fixed

--- compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirOldFrontendDiagnosticsTestGenerated.java
@@ -10981,6 +10981,11 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirOldFronte
                 runTest("compiler/testData/diagnostics/tests/inference/nothingType/inferArgumentToNothingFromNullConstant.kt");
             }
 
+            @TestMetadata("inferenceWithRecursiveGenericsAndNothing.kt")
+            public void testInferenceWithRecursiveGenericsAndNothing() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/inference/nothingType/inferenceWithRecursiveGenericsAndNothing.kt");
+            }
+
             @TestMetadata("kt24490.kt")
             public void testKt24490() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/inference/nothingType/kt24490.kt");
